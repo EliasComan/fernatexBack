9f847e06050f3c6f9869cdc2b88cabc92f4287d8
eliasComan <comanelias5@gmail.com>
2023-09-26 23:24:48 -0300

    pruebas

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from 'dotenv'
 import cors from 'cors'
 import bodyParser from "body-parser";
+import {createTransport} from 'nodemailer'
 
 
 dotenv.config()
@@ -14,8 +15,40 @@ app.get('/', (req,res) => {
     res.send('HELLO WORDL')
 })
 
-app.post('/', (req, res) => {
-    console.log(req.body) 
+app.post('/', async (req, res) =>  {
+    const nodemailer={
+        user: 'comanelias5@gmail.com',
+        pass: 'puviydbskzmdxebo'
+}
+const transporter = createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    auth: {
+        user: nodemailer.user,
+        pass: nodemailer.pass 
+    }
+})
+    const{ nombre, telefono, email, coment } = req.body
+    console.log(nombre, telefono,email,coment) 
+    await transporter.sendMail({
+        from: "FERNATEX APP <NORESPONDER@FERNATEX.com>",
+        to: `comanelias5@gmail.com`,
+        subject: "ENVIO NUEVA CONSULTA DESDE PAGINA WEB",
+        text: `MENSAJE DE PRUEBA
+        NOMBRE: ${nombre},
+        TELEFONO : ${telefono}
+        EMAIL: ${email}
+        COMENT: ${coment}
+        `,
+        html: `
+       
+        <p>NOMBRE: ${nombre},
+        TELEFONO : ${telefono}
+        EMAIL: ${email}
+        COMENT: ${coment}
+        </p>
+        `,
+      });
     res.send('Done')
 })
 
